ddb4e39c0871759fbb612925ae98364a3b63b00b
mehdibellagrini-eng <mehdibellagrini@gmail.com>
2026-04-19 23:18:54 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { unoptimized: true },
+  async rewrites() {
+    return [
+      {
+        source: '/tiktokMzUvvCFTrtgK1duCaIUHrIDrTKbcieLX.txt',
+        destination: '/publique/tiktokMzUvvCFTrtgK1duCaIUHrIDrTKbcieLX.txt',
+      },
+      {
+        source: '/tiktokfPaPfxD02QOVrmbaX1CnL0f8ytv3DOGv.txt',
+        destination: '/publique/tiktokfPaPfxD02QOVrmbaX1CnL0f8ytv3DOGv.txt',
+      },
+    ];
+  },
 };
 module.exports = nextConfig;
